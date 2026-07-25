bf06794a4b601b84dc8392d21eee6224aa9aba38
Juan Pablo Valle Matheu <juanpvalle0608@gmail.com>
2026-07-25 09:41:54 -0600

    Primeros 2 metodos: primero disponible y buscar pasajeros.

--- JavaApplication90/src/PalindromoAir.java
+++ JavaApplication90/src/PalindromoAir.java
@@ -1,13 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author oscar
- */
 public class PalindromoAir {
-    
+
+    Ticket[] asientos = new Ticket[30];
+
+    public int firstAvailable(int i) {
+        if (i >= 30) return -1;
+        if (asientos[i] == null) return i;
+        return firstAvailable(i + 1);
+    }
+
+    public int searchPassenger(String name, int i) {
+        if (i >= 30) return -1;
+        if (asientos[i] != null && asientos[i].getNombre().equals(name)) return i;
+        return searchPassenger(name, i + 1);
+    }
+
+    public Ticket[] getAsientos() {
+        return asientos;
+    }
 }
